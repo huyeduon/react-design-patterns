ecbd92489f2fb9a7dd2d318d26d2ffdc9200aa1c
huyeduon <huyeduon@cisco.com>
2024-04-17 01:59:36 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,5 @@
-import { SplitScreen } from './SplitScreen';
-import './App.css';
-const LeftHandComponent = () => {
-  return <h1 style={{ backgroundColor: 'green' }}>Left</h1>;
-};
-
-const RightHandComponent = () => {
-  return <h1 style={{ backgroundColor: 'red' }}>Right</h1>;
-};
 function App() {
-  return (
-    <SplitScreen leftWeight={1} rightWeight={3}>
-      <LeftHandComponent />
-      <RightHandComponent />
-    </SplitScreen>
-  );
+  return <h1>Your code goes here</h1>;
 }
 
 export default App;
