c354e3d7f66ff2cceacba48773fadd156cdc4c55
huyeduon <huyeduon@cisco.com>
2024-04-18 20:34:45 +0800

    controlled onboarding

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
-import { SplitScreen } from './SplitScreen';
 import './App.css';
-const LeftHandComponent = () => {
-  return <h1 style={{ backgroundColor: 'green' }}>Left</h1>;
-};
+import { useState } from 'react';
+import { ControlledOnboardingFlow } from './ControlledOnboardingFlow';
 
-const RightHandComponent = () => {
-  return <h1 style={{ backgroundColor: 'red' }}>Right</h1>;
-};
+const StepOne = ({ goToNext }) => (
+  <>
+    <h1>Step 1</h1>
+    <button onClick={() => goToNext({ name: 'John Doe' })}>Next</button>
+  </>
+);
+
+const StepTwo = ({ goToNext }) => (
+  <>
+    <h1>Step 2</h1>
+    <button onClick={() => goToNext({ age: 50 })}>Next</button>
+  </>
+);
+
+const StepThree = ({ goToNext }) => (
+  <>
+    <h1>Step 3</h1>
+    <p>Congratulations! You qualify for our senior discount. </p>
+    <button onClick={() => goToNext()}>Next</button>
+  </>
+);
+
+const StepFour = ({ goToNext }) => (
+  <>
+    <h1>Step 4</h1>
+    <button onClick={() => goToNext({ hairColor: 'brown' })}>Next</button>
+  </>
+);
+
 function App() {
+  const [onboardingData, setOnboardingData] = useState({});
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const onNext = (stepData) => {
+    setOnboardingData({
+      ...onboardingData,
+      ...stepData,
+    });
+    setCurrentIndex(currentIndex + 1);
+  };
   return (
-    <SplitScreen leftWeight={1} rightWeight={3}>
-      <LeftHandComponent />
-      <RightHandComponent />
-    </SplitScreen>
+    <ControlledOnboardingFlow currentIndex={currentIndex} onNext={onNext}>
+      <StepOne />
+      <StepTwo />
+      {onboardingData.age >= 62 && <StepThree />}
+      <StepFour />
+    </ControlledOnboardingFlow>
   );
 }
 
